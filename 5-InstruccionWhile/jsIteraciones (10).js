3b0a4276903f57bed306fdc10067f213e8ba84a7
Jose-M-Acosta <yanojr550@gmail.com>
2020-02-20 01:03:10 -0300

    19/02/20

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,17 +1,62 @@
-function mostrar()
-{
+function mostrar() {
 
-	var contador=0;
-	//declarar contadores y variables 
-	
-	var respuesta="si";
+	var respuesta = "s";
+	var numIngresado;
+	var sumaPosi = 0;
+	var sumaNegi = 0;
+	var conPosi = 0;
+	var conNegi = 0;
+	var conCero = 0;
+	var conPar = 0;
+	var proPosi = 0;
+	var proNegi = 0;
+	var differencia = 0;
 
-	while(respuesta!="no")
-	{
-		
-	
-	}
 
+	do {
+		numIngresado = parseInt(prompt('Ingrese un numero: '));
+
+		while (isNaN(numIngresado)) {
+			numIngresado = parseInt(prompt('No es un numero! Ingrese un numero: '));
+		}
+
+		if (numIngresado / 2 == 0) {
+			conPar++;
+		}
+
+		if (numIngresado < 0) {
+			sumaNegi = sumaNegi + numIngresado;
+			conNegi++;
+		}
+
+		else if (numIngresado > 0) {
+			sumaPosi = sumaPosi + numIngresado;
+			conPosi++;
+		}
+
+		else {
+			conCero++;
+		}
+
+		respuesta = prompt('desea Ingresar otro numero? ');
+	} while (respuesta == 's');
+
+
+	proPosi = sumaPosi / conPosi;
+	proNegi = sumaNEgi / conNegi;
+
+	differencia = sumaPosi - sumaNegi;
+
+
+	document.write('1.Suma de los Negativos: ' + sumaNegi + '</br>');
+	document.write('2.Suma de los Positivos: ' + sumaPosi + '</br>');
+	document.write('3.Cantidad de Positivos: ' + conPosi + '</br>');
+	document.write('4.Cantidad de Negativos: ' + conNegi + '</br>');
+	document.write('5.Cantidad de Ceros: ' + conCero + '</br>');
+	document.write('6.Cantidad de numeros pares: ' + conPar + '</br>');
+	document.write('7.Promedio de los Positivos: ' + proPosi + '</br>');
+	document.write('8.Promedio de los Negativos: ' + proNegi + '</br>');
+	document.write('9.Differencia de los Positivos y los Negativos: ' + sumaNegi + '</br>');
 
 
 
