2423501fc7bffe16506af47b0ddb007ab9c20682
Jose Acosta <yanojr550@gmail.com>
2020-03-03 17:20:33 -0300

    03/03/2020

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,74 @@
-function mostrar()
-{
+function mostrar() {
+    var letra;
+    var numero;
+    var continuar;
+    var conPares = 0;
+    var conImpar = 0;
+    var cantidadZero = 0;
+    var promedioPosi;
+    var numPosi = 0;
+    var acumuladorPositivo = 0;
+    var numNeg = 0;
+    var acumuladorNegativo = 0;
+    var flagMax = 0;
+    var flagMin = 0;
+    var numMax = 0;
+    var numMin = 0;
+    var letraMin;
+    var letraMax;
 
+
+    do {
+        letra = prompt('Ingrese una letra ');
+
+        numero = parseInt(prompt('Ingrese un numero'));
+        while (isNaN(numero) || numero > 100 || numero < -100) {
+            numero = parseInt(prompt('no es un numero ingrese. Ingrese un numero valido.'));
+        }
+
+        if (numero % 2 == 0) {
+            conPares++;
+        } else {
+            conImpar++;
+        }
+
+        if (numero == 0) {
+            cantidadZero++;
+        }
+
+        if (numero > 0) {
+            numPosi++;
+            acumuladorPositivo = acumuladorPositivo + numero;
+        }
+
+        if (numero < 0) {
+            numNeg++;
+            acumuladorNegativo = acumuladorNegativo + numero
+        }
+
+        if (flagMax == 0 || numero > numMax) {
+            numMax = numero;
+            letraMax = letra;
+            flagMax = 1;
+        }
+
+        if (flagMin == 0 || numero < numMin) {
+            numMin = numero;
+            letraMin = letra;
+            flagMin = 1;
+        }
+
+        continuar = prompt('Desea ingresar otro numero?');
+
+    } while (continuar == 's');
+
+    promedioPosi = acumuladorPositivo / numPosi;
+
+    document.write('1.Cantidad de numeros pares: ' + conPares + '</br>');
+    document.write('2.Cantidad de numeros impares: ' + conImpar + '</br>');
+    document.write('3.Cantidad de zeros: ' + cantidadZero + '</br>');
+    document.write('4.Promedio de numeros positivos: ' + promedioPosi + '</br>');
+    document.write('5. La suma de todos los números negativos: ' + acumuladorNegativo + '</br>');
+    document.write('6.El numero y letra maxima es: ' + numMax + letraMax+ '</br>');
+    document.write('7.El numero y letra minima es :' + numMin + letraMin);
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,9 +1,14 @@
 function mostrar() {
     var nota;
     var sexo;
+    var promedio;
+    var acumuladorNota = 0;
+    var notaMenor;
+    var flag = 0;
+    var sexoMenor;
+    var contadorVaronesMas6 = 0;
 
-
-    for (var i = 0; i <= 5; i++) {
+    for (var i = 0; i < 5; i++) {
 
         nota = parseInt(prompt('Ingrese el numero de la Nota!'));
         while (isNaN(nota) || nota < 0 || nota > 10) {
@@ -14,5 +19,22 @@ function mostrar() {
         while (sexo != 'f' && sexo != 'm') {
             sexo = prompt('No es un Sexo valido! Ingrese "f" o "m"!');
         }
+
+        acumuladorNota = acumuladorNota + nota;
+
+        if (nota < notaMenor || flag == 0) {
+            notaMenor = nota;
+            sexoMenor = sexo;
+            flag = 1;
+        }
+
+        if (sexo == 'm' && nota >= 6) {
+            contadorVaronesMas6++;
+        }
     }
+
+    promedio = acumuladorNota / 5;
+
+    alert("Promedio Notas: " + promedio + "\nNota Baja: " + notaMenor + "\nSexo nota baja: " + sexoMenor + "\nContador Varones nota >6: " + contadorVaronesMas6);
 }
+
