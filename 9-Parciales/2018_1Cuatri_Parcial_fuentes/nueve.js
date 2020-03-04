3b3d9f4b0255ac15612f914b9e984d08f284831c
Jose-M-Acosta <yanojr550@gmail.com>
2020-03-04 01:25:09 -0300

    03/03/2020

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,76 @@
-function mostrar()
-{
+function mostrar() {
+    var marca;
+    var peso;
+    var temperatura;
+    var continuar;
+    var conTempPar = 0;
+    var pesoPesado = 0;
+    var marcaPesado;
+    var productoCongelado = 0;
+    var pesoMax = 0;
+    var pesoMin = 0;
+    var flag2 = 0;
+    var promedio;
+    var pesoAcumulado = 0;
+    var conPeso = 0;
+
+    do {
+        marca = prompt('Ingrese la marca del producto.');
+
+        peso = parseInt(prompt('Ingrese el peso del producto.(1-100)'));
+        while (isNaN(peso) || peso < 1 || peso > 100) {
+            peso = parseInt(prompt('Dato invalido. Ingrese el peso del producto. (1-100)'));
+        }
+
+        temperatura = parseInt(prompt('Ingrese temperatura.(-30 hasta 30)'));
+        while (isNaN(temperatura) || temperatura < -30 || temperatura > 30) {
+            temperatura = parseInt(prompt('Dato invalido. Ingrese temperatura (-30 hasta 30)'));
+        }
+
+        if (temperatura % 2 == 0) {
+            conTempPar++;
+        }
+
+        if ((temperatura > 0) && (flag1 = 0 || peso < pesoPesado)) {
+            pesoPesado = peso;
+            marcaPesado = marca;
+            flag1 = 1;
+        }
+
+        if (temperatura < 0) {
+            productoCongelado++;
+        }
+
+        if (peso < pesoMin || flag2 == 0) {
+            pesoMin = peso;
+        }
+
+        if (peso > pesoMax || flag2 == 0) {
+            pesoMax = peso;
+            flag2 = 1;
+        }
+
+        pesoAcumulado = pesoAcumulado + peso;
+        conPeso++;
+
+        continuar = prompt('Usted desea continuar? ');
+    } while (continuar == 's');
+
+    while (conPeso != 0) {
+        promedio = pesoAcumulado / conPeso;
+    }
+
+
+    document.write('1.La cantidad de temperatura pares: ' + conTempPar + '</br>');
+    document.write('2.La marca del producto mas pesado no congelado: ' + marcaPesado + '</br>');
+    document.write('3.La cantidad de productos que se consercan a menos de 0 grados: ' + productoCongelado + '</br>');
+    document.write('4.El promedio del peso de todos los productos: ' + promedio + '</br>');
+    document.write('2.El peso maximo: ' + pesoMax + ' El peso minimo' + pesoMin + '</br>');
 
 }
+/*
+a) La cantidad de temperaturas pares. 
+b) La marca del producto más pesado 
+c) La cantidad de productos que se conservan a menos de 0 grados. 
+d) El promedio del peso de todos los productos.	
+f) El peso máximo y el mínimo.
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -5,7 +5,7 @@ function mostrar() {
     var conPares = 0;
     var conImpar = 0;
     var cantidadZero = 0;
-    var promedioPosi;
+    var promedioPosi = 0;
     var numPosi = 0;
     var acumuladorPositivo = 0;
     var numNeg = 0;
@@ -16,14 +16,20 @@ function mostrar() {
     var numMin = 0;
     var letraMin;
     var letraMax;
+    var letraMinu;
+    var numeroMinu = 0;
+    var flagMinu = 0;
 
 
     do {
         letra = prompt('Ingrese una letra ');
+        while (!((letra >= 'A' && letra <= 'Z') || (letra >= 'a' && letra <= 'z'))) {
+            letra = prompt('Dato no valido. Ingrese una letra.')
+        }
 
-        numero = parseInt(prompt('Ingrese un numero'));
+        numero = parseInt(prompt('Ingrese un numero entre (-100 y 100). '));
         while (isNaN(numero) || numero > 100 || numero < -100) {
-            numero = parseInt(prompt('no es un numero ingrese. Ingrese un numero valido.'));
+            numero = parseInt(prompt('No es un numero valido. Ingrese un numero valido.'));
         }
 
         if (numero % 2 == 0) {
@@ -58,17 +64,29 @@ function mostrar() {
             flagMin = 1;
         }
 
+        if (letra <= 'z' && letra >= 'a') {
+            if (numero < numeroMinu || flagMinu == 0) {
+                numeroMinu = numero;
+                letraMinu = letra;
+                flagMinu = 1;
+            }
+        }
+
+
         continuar = prompt('Desea ingresar otro numero?');
 
     } while (continuar == 's');
 
-    promedioPosi = acumuladorPositivo / numPosi;
+    while (numposi != 0) {
+        promedioPosi = acumuladorPositivo / numPosi;
+    }
 
     document.write('1.Cantidad de numeros pares: ' + conPares + '</br>');
     document.write('2.Cantidad de numeros impares: ' + conImpar + '</br>');
     document.write('3.Cantidad de zeros: ' + cantidadZero + '</br>');
     document.write('4.Promedio de numeros positivos: ' + promedioPosi + '</br>');
-    document.write('5. La suma de todos los números negativos: ' + acumuladorNegativo + '</br>');
-    document.write('6.El numero y letra maxima es: ' + numMax + letraMax+ '</br>');
-    document.write('7.El numero y letra minima es :' + numMin + letraMin);
+    document.write('5.La suma de todos los números negativos: ' + acumuladorNegativo + '</br>');
+    document.write('6.El numero maxima es: ' + numMax + ' y su letra es: ' + letraMax + '</br>');
+    document.write('7.El numero minima es :' + numMin + ' y su letra es: ' + letraMin + '</br>');
+    document.write('8.El numero menor de letra miniscula es: ' + numeroMinu + ' y su letra es: ' + letraMinu);
 }
